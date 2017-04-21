8b77fdfbeda57af64cf1e71b80389ef49e6d59ad
William Parker <wparker30@yahoo.com>
2017-04-21 16:25:40 -0500

    Prevent the font size of line numbers from increasing so much that the numbers are no longer visible

--- Preferences.el
+++ Preferences.el
@@ -41,8 +41,24 @@
 
 (require 'rainbow-delimiters)
 
-;;;; General setup
+;;;; Linum setup
 (global-linum-mode t)
+
+;; Prevent the font size of line  numbers from increasing excessively when the buffer font
+;; size increases.  The fix comes from https://unix.stackexchange.com/a/237023.
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+          (ceiling (* (if (boundp 'text-scale-mode-step)
+                  (expt text-scale-mode-step
+                    text-scale-mode-amount) 1)
+              (if (car (window-margins))
+                  (car (window-margins)) 1)
+              ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
+
+;;;; Miscellaneous setup
+
 (require 'smooth-scrolling)
 (require 'neotree)
 (setq neo-window-width 90)
